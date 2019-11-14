b4cc14735d699c1eea2a0f4fb56ecd36bda28edd
Nethelbert <gilblasse@gmail.com>
2019-11-14 14:18:01 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist == self
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.all.select {|artist| artist.name == name}
+  end
+  
+  
+end
