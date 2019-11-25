4578b1c9904f27db696194a52edf48e922d9fa74
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-25 03:54:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,15 +18,16 @@ class Song
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song.save
+    new_song
   end
   
   def self.find_by_artist(artist)
     Song.all.select{|song| song.artist == artist}
   end
   
-  def artist_name=(name =)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    binding.pry
+    #binding.pry
   end
   
   def save
