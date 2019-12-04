313042fc01876444d8033c8d14f041d847e398e9
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2019-12-03 17:54:50 -0800

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,10 @@ attr_accessor :name, :artist
 
 
     def self.new_by_filename(filename)
-        song_name = filename.split(" - ")[1]
-        artist = filename.split(" - ")[0]
-        song = self.new(song_name)
-        song.artist_name = artist
-        song
+        artist, song = filename.split(" - ")
+        new_song = self.new(song)
+        new_song.artist_name = artist
+        new_song
     end 
 
 
